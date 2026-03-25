3e266346b55fdbb0075b3b772f36e19b24cd7459
thaissa <thaissa.leslye.tl@gmail.com>
2026-03-25 10:05:55 -0300

    add profile route

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 //app/pages/home.ts
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { UiAvatar } from '../../components/ui-avatar/ui-avatar';
 
 @Component({
   selector: 'app-home',
-  imports: [UiAvatar],
+  imports: [UiAvatar,RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
